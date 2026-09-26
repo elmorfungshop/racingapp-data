276c92431b1486e6c49a51bf9fe8b37048920045
elmorfungshop <elmorfungshop@gmail.com>
2026-09-26 17:50:15 +0100

    Clean up local and remove unnecessary files

--- update_github - 複製.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d C:\Users\Winnie\Desktop\racingapp
-git add data.json
-git commit --amend -m "Auto update racing data"
-git push -f origin main
--- update_github.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d C:\Users\Winnie\Desktop\racingapp
-git status
-git add data.json
-git commit -m "Auto update data.json"
-git push origin main
-pause
